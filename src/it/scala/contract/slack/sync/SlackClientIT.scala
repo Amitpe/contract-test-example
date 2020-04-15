4a528e535882a0f1e508f0317b9b1219463a0149
amitp <amitp@wix.com>
2020-04-15 17:02:19 +0300

    cleanup

--- src/it/scala/contract/slack/sync/SlackClientIT.scala
+++ src/it/scala/contract/slack/sync/SlackClientIT.scala
@@ -3,12 +3,10 @@ package contract.slack.sync
 import contract.slack.testkit.FakeSlackServer
 import org.specs2.specification.BeforeAll
 
-import scala.util.Random
-
 class SlackClientIT extends SlackClientContract with BeforeAll {
 
   private val port = 11111
-  private val token = Random.nextString(10)
+  private val token = aRandomString
 
   override protected val existingChannels = Seq(aChannel)
   override protected val baseUrl = s"http://localhost:$port"
